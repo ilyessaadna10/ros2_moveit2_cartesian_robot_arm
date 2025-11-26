0b59fbb0d3067e1d07ec5984602343b8e62dcc17
ilyes <ilyessaadna4@gmail.com>
2025-11-26 11:05:07 +0100

    Remove build, install, gantry_moveit_config, and other ignored folders from tracking

--- build/gantry/CMakeFiles/gantry_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/gantry_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/gantry_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/gantry_moveit_config/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ilyaes/Desktop/ros2project/moveit2_tuto/ros2_gantry_ws/src/gantry_moveit_config
-# Build directory: /home/ilyaes/Desktop/ros2project/moveit2_tuto/ros2_gantry_ws/build/gantry_moveit_config
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
